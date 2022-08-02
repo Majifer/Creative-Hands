a86378af8b18daac3081e1657af15215001edf15
Majifer <fjmario99@gmail.com>
2022-08-02 18:33:47 +0200

    Template for Creative Hands Project

---
+++ Creative Hands/Creative-Hands/src/components/Majifer.vue
@@ -0,0 +1,20 @@
+
+<template>
+    <div class="Majifer-tittle">
+        <h3>
+            Hola Exito. has llegado hasta aqui
+        </h3>
+
+        <h4>
+            Suerte a partir de ahora!!!
+            <p>Hasta ahora todo va demasiado bien por lo que tenemos que tener algun tipo de problema en el transcurso de los acaonteicmientso </p>
+
+            <li>
+                <ol>1</ol>
+                <ol>2</ol>
+                <ol>3</ol>
+            </li>
+        </h4>
+
+    </div>
+</template>
